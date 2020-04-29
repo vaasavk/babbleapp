9ffec3b7bcc6cf3395e3553a3cbbb66cd21b3fc0
vaasavk <vaasavk@vt.edu>
2020-04-28 21:08:11 -0400

    edits in docker.compose.yml

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import pymongo as database
 import time
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_client import Counter
+import os
 
 app = Flask(__name__)
+
+path = os.getenv('EnvVarPath')
+
+load_dotenv(dotenv_path=path)
+mongoHost = os.getenv('DATABASE_HOST', 'mongo')
+mongoPort = os.getenv('DATABASE_PORT', '27017')
+
 metrics = PrometheusMetrics(app)
 totalBlabsCreated = Counter("AllBlabsCounter", "The total amount of blabs that have ever been created")
 totalBlabsRemoved = Counter("BlabsRemoved", "The total amount of blabs that have been deleted")
 totalBlabsGotten = Counter("BlabsGotten", "The total number of times get blabs has been called")
-mongoClient = database.MongoClient("mongodb://mongo:27017")
+mongoClient = database.MongoClient("mongodb://%s:%s" % (mongoHost, mongoPort))
 mongoBabbles = mongoClient["babble"]["blabs"]
 
 blabs = []
